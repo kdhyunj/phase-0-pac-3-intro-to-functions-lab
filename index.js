9aa63a3a4646bc41650fce5e050638189d401783
Hyunjoon Kim <kdhyunj@gmail.com>
2022-12-16 10:01:14 -0500

    commit tree a5d7884559baa85a121822e18aa64c5f0356f794

---
+++ index.js
@@ -0,0 +1,5 @@
+function shout(string) {return string.toUpperCase();}
+function whisper(string) {return string.toLowerCase();}
+function logShout(string){console.log(string.toUpperCase());}
+function logWhisper(string){console.log(string.toLowerCase());}
+function sayHiToHeadphonedRoommate(string) {return string.toLowerCase(), return string.toUpperCase(), return string.toEqual();}
